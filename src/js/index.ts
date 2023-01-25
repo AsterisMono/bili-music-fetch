abe20d8e592980443ed5cdf16bc39f8970472f16
Chatnoir Miki <cmiki@amono.me>
2023-01-25 21:05:47 +0800

    Tidify debug section Switch to zhihu unpkg mirror

--- src/js/index.ts
+++ src/js/index.ts
@@ -8,16 +8,16 @@ const bvidInput = document.querySelector(".bvid_input") as HTMLInputElement;
 const submitButton = document.querySelector(".submit") as HTMLButtonElement;
 
 const ffmpeg = createFFmpeg({
-  // corePath: new URL("ffmpeg-core.js", document.location.toString()).href,
+  corePath: "https://unpkg.zhimg.com/@ffmpeg/core@0.11.0/dist/ffmpeg-core.js",
   log: true,
 }) as FFmpeg;
 
-// submitButton.addEventListener("click", () => {
-//   transcodeBiliVideo(ffmpeg, bvidInput.value, nextStatus, setProgress);
-// });
+submitButton.addEventListener("click", () => {
+  transcodeBiliVideo(ffmpeg, bvidInput.value, nextStatus, setProgress);
+});
 
 (async () => {
   await ffmpeg.load();
   nextStatus("等待指令");
-  // submitButton.disabled = false;
+  submitButton.disabled = false;
 })();
