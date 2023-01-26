d63d77fa64bd67ab7ebbfa35a13629537378d840
Chatnoir Miki <cmiki@amono.me>
2023-01-26 12:58:11 +0800

    Implement download progress

--- src/js/transcodeBiliVideo.ts
+++ src/js/transcodeBiliVideo.ts
@@ -4,10 +4,10 @@ export const transcodeBiliVideo = async function (
   ffmpeg: FFmpeg,
   bvid: string,
   statusCb: (statusMsg: string) => Promise<void>,
-  ratioCb: (ratio: number) => Promise<void>
+  progressCb: (ratio: number) => Promise<void>
 ) {
   // 1. fetch video info
-  ratioCb(0.2);
+  progressCb(0.1);
   await statusCb("正在获取视频信息");
   const infoResult = await fetch(
     `https://bili-fetch.nanekino8629.workers.dev/video/${bvid}`
@@ -20,14 +20,18 @@ export const transcodeBiliVideo = async function (
     `${bvid}_${cid}_${stage}.${ext}`;
 
   // 2. download video
-  ratioCb(0.4);
+  progressCb(0.2);
   await statusCb("正在下载视频");
-  ffmpeg.FS("writeFile", getFileName("raw", "mp4"), await fetchFile(videoUrl));
+  ffmpeg.FS(
+    "writeFile",
+    getFileName("raw", "mp4"),
+    await fetchFile(videoUrl, (ratio) => progressCb(0.2 + ratio * 0.3))
+  );
 
   // 3. convert video to audio
-  ratioCb(0.6);
+  progressCb(0.5);
   await statusCb("正在进行转换");
-  ffmpeg.setProgress(({ ratio }) => ratioCb(0.6 + ratio * 0.2));
+  ffmpeg.setProgress(({ ratio }) => progressCb(0.5 + ratio * 0.3));
   await ffmpeg.run(
     "-i",
     getFileName("raw", "mp4"),
@@ -40,12 +44,12 @@ export const transcodeBiliVideo = async function (
   ffmpeg.setProgress(() => undefined);
 
   // 4. bundle metadata
-  ratioCb(0.8);
+  progressCb(0.8);
   await statusCb("正在给文件附魔");
   ffmpeg.FS(
     "writeFile",
     getFileName("albumart", "jpg"),
-    await fetchFile(coverImageUrl)
+    await fetchFile(coverImageUrl, (ratio) => progressCb(0.8 + ratio * 0.1))
   );
   await ffmpeg.run(
     "-i",
@@ -72,7 +76,7 @@ export const transcodeBiliVideo = async function (
   );
 
   // 5. finish up
-  ratioCb(1.0);
+  progressCb(1.0);
   await statusCb("即将完成...");
   offerFileAsDownload(ffmpeg, getFileName("out", "mp3"), `${videoTitle}.mp3`);
 };
--- src/js/util.ts
+++ src/js/util.ts
@@ -1,11 +1,52 @@
 import type { FFmpeg } from "@ffmpeg/ffmpeg";
 
-export const fetchFile = async (URL: string) => {
-  const res = await fetch(URL, {
-    referrer: "",
-  });
-  const data = await res.arrayBuffer();
-  return new Uint8Array(data);
+export const fetchFile = async (
+  URL: string,
+  progressCb: (ratio: number) => Promise<void>
+) => {
+  try {
+    const res = await fetch(URL, {
+      referrer: "",
+    });
+    if (!res.ok) throw makeError("下载失败", `${res.status} ${res.statusText}`);
+    if (!res.body) throw makeError("下载失败", "请使用现代浏览器");
+    const contentEncoding = res.headers.get("content-encoding");
+    const contentLength = res.headers.get(
+      contentEncoding ? "x-file-size" : "content-length"
+    );
+    const total = parseInt(contentLength!, 10);
+    let loaded = 0;
+    const progressedResponse = new Response(
+      new ReadableStream({
+        start(controller) {
+          const reader = res.body!.getReader();
+          read();
+          function read() {
+            reader
+              .read()
+              .then(({ done, value }) => {
+                if (done) {
+                  controller.close();
+                  return;
+                }
+                loaded += value.byteLength;
+                progressCb(loaded / total);
+                controller.enqueue(value);
+                read();
+              })
+              .catch((error) => {
+                console.error(error);
+                controller.error(error);
+              });
+          }
+        },
+      })
+    );
+    const data = await progressedResponse.arrayBuffer();
+    return new Uint8Array(data);
+  } catch (e) {
+    throw makeError("下载失败", "出现了意料之外的错误", e);
+  }
 };
 
 export function offerFileAsDownload(
@@ -21,4 +62,12 @@ export function offerFileAsDownload(
   document.body.appendChild(downloadEl);
   downloadEl.click();
   document.body.removeChild(downloadEl);
+}
+
+export function makeError(
+  status: string,
+  detailMsg: string,
+  originalError?: Error
+) {
+  throw { status, detailMsg, originalError };
 }
